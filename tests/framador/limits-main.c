d5bc5d0e6c8d31faacc42985b10c4683b38a7aa7
Mal <malory@onenetbeyond.org>
2025-06-10 15:39:45 +0100

    working on it

--- tests/framador/limits-main.c
+++
@@ -1,9 +0,0 @@
-#include <ft_printf.h>
-#include <limits.h>
-int	main()
-{
-	printf("%d\n", ft_printf("\nmine:%p\n%p\n", LONG_MIN, LONG_MAX));
-	printf("%d\n", printf("\noriginal:%p\n%p\n", LONG_MIN, LONG_MAX));
-	printf("%d\n", ft_printf("\nmine:%p\n%p\n", ULONG_MAX, -ULONG_MAX));
-	printf("%d\n", printf("\noriginal%p\n%p\n", ULONG_MAX, -ULONG_MAX));
-}
